d9ba96062803ab305416d7969c515b94cb5d2c10
Alfred Broda <alfred.broda@gmail.com>
2018-01-30 11:41:48 +0100

    Use executorId instead of generating UUID for metrics

--- metrics/graphite_test.go
+++ metrics/graphite_test.go
@@ -28,7 +28,6 @@ func TestIfNotFailsToSetupGraphiteWithValidConfig(t *testing.T) {
 }
 
 func TestIfBuildsCorrectMetricsPrefix(t *testing.T) {
-	metricsUUID = "uuid"
 	testCases := []struct {
 		hostname       string
 		expectedPrefix string
@@ -42,7 +41,7 @@ func TestIfBuildsCorrectMetricsPrefix(t *testing.T) {
 			os.Setenv("MESOS_HOSTNAME", tc.hostname)
 			defer os.Unsetenv("MESOS_HOSTNAME")
 
-			actualPrefix := buildUniquePrefix("basePrefix")
+			actualPrefix := buildUniquePrefix("basePrefix", "uuid")
 			assert.Equal(t, tc.expectedPrefix, actualPrefix)
 		})
 	}
