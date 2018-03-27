8802d45c7ff5b46e4c81cdcbca96e76e5a246ae6
Tomasz Janiszewski <tomasz.janiszewski@allegro.pl>
2018-03-27 15:35:48 +0200

    Extract common hook creation code

--- cmd/executor/main.go
+++ cmd/executor/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"time"
 
@@ -25,11 +24,7 @@ var Version string
 // Config contains application configuration
 var Config executor.Config
 
-var debug = *flag.Bool("debug", false, "Forces executor debug mode")
-
 func init() {
-	flag.Parse()
-
 	if err := envconfig.Process(executor.EnvironmentPrefix, &Config); err != nil {
 		log.WithError(err).Fatal("Failed to load executor configuration")
 	}
@@ -38,8 +33,7 @@ func init() {
 		log.WithError(err).Fatal("Failed to initialize Sentry")
 	}
 
-	if Config.Debug || debug {
-		Config.Debug = true
+	if Config.Debug {
 		log.SetLevel(log.DebugLevel)
 	} else {
 		log.SetLevel(log.InfoLevel)
@@ -85,24 +79,26 @@ func initSentry(config executor.Config) error {
 
 func createHooks() []hook.Hook {
 	var consulConfig consul.Config
-	if err := envconfig.Process(executor.EnvironmentPrefix, &consulConfig); err != nil {
-		log.WithError(err).Fatal("Failed to load Consul hook configuration")
-	}
+	readConfiguration(&consulConfig)
 	consulHook, err := consul.NewHook(consulConfig)
 	if err != nil {
 		log.WithError(err).Fatalf("Error loading Consul hook %s", err)
 	}
 
 	var vaasConfig vaas.Config
-	if err := envconfig.Process(executor.EnvironmentPrefix, &vaasConfig); err != nil {
-		log.WithError(err).Fatal("Failed to load VaaS hook configuration")
-	}
+	readConfiguration(&vaasConfig)
 	vaasHook, err := vaas.NewHook(vaasConfig)
 	if err != nil {
 		log.WithError(err).Fatalf("Error loading VaaS service hook %s", err)
 	}
 
-	return []hook.Hook{consulHook, vaasHook}
+	return []hook.Hook{vaasHook, consulHook}
+}
+
+func readConfiguration(config interface{}) {
+	if err := envconfig.Process(executor.EnvironmentPrefix, config); err != nil {
+		log.WithError(err).Fatal("Failed to load configuration")
+	}
 }
 
 func main() {
@@ -111,8 +107,17 @@ func main() {
 	if err != nil {
 		log.WithError(err).Fatal("Failed to load Mesos configuration")
 	}
-	Config.MesosConfig = cfg
 	metrics.Init(cfg.ExecutorID)
+	// TODO(janisz): Use custom type for configuration
+	Config.MesosConfig.FrameworkID = cfg.FrameworkID
+	Config.MesosConfig.ExecutorID = cfg.ExecutorID
+	Config.MesosConfig.Directory = cfg.Directory
+	Config.MesosConfig.Sandbox = cfg.Sandbox
+	Config.MesosConfig.AgentEndpoint = cfg.AgentEndpoint
+	Config.MesosConfig.ExecutorShutdownGracePeriod = cfg.ExecutorShutdownGracePeriod
+	Config.MesosConfig.Checkpoint = cfg.Checkpoint
+	Config.MesosConfig.RecoveryTimeout = cfg.RecoveryTimeout
+	Config.MesosConfig.SubscriptionBackoffMax = cfg.SubscriptionBackoffMax
 	exec := executor.NewExecutor(Config, createHooks()...)
 	if err := exec.Start(); err != nil {
 		log.WithError(err).Fatal("Executor exited with error")
