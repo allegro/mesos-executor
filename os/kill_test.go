71d234683b454e282b0b0dee17840b489d631038
Piotr Betkier <pbetkier@gmail.com>
2019-11-26 12:27:05 +0100

    Fix KillTree tests: missing assertion and separate pgid for test processes (#134)

--- os/kill_test.go
+++ os/kill_test.go
@@ -6,12 +6,16 @@ import (
 	"os/exec"
 	"syscall"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+const TIME_LIMIT = 5 * time.Second // equal to sleep time in test scripts
+
 func TestSendingSignalToTree(t *testing.T) {
+	startTime := time.Now()
 	cmd := exec.Command("testdata/fork.sh")
 	err := cmd.Start()
 	require.NoError(t, err)
@@ -21,6 +25,14 @@ func TestSendingSignalToTree(t *testing.T) {
 	_, _ = cmd.Process.Wait()
 
 	assert.False(t, processExists(cmd.Process.Pid))
+	assertFinishedWithinTimeLimit(t, startTime)
+}
+
+// Test processes finish successfully after TIME_LIMIT. Only if the test finishes earlier
+// can we be sure that the processes were indeed killed.
+func assertFinishedWithinTimeLimit(t *testing.T, startTime time.Time) {
+	assert.True(t, time.Now().Before(startTime.Add(TIME_LIMIT)),
+		"took longer than %v, test processes were probably not killed", TIME_LIMIT)
 }
 
 func processExists(pid int) bool {
